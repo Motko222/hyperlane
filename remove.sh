3e033c3a08a4b4fa56b1a3b8b6e052085925a415
Motko <77466238+Motko222@users.noreply.github.com>
2025-04-04 13:27:30 +0200

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+
+docker stop $folder
+docker rm $folder
+
+rm -r /root/hyperlane_db_$CHAIN
+mv /root/scripts/$folder /root/backup
